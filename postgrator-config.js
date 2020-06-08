33e6186c45ae3c5e94994a6d198388e4f54240a9
Malcolm Kiano <malcolmkiano@gmail.com>
2020-06-08 16:58:16 -0400

    fix: add password fallback

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,12 +4,12 @@ module.exports = {
   ssl: !!process.env.SSL,
   migrationsDirectory: 'migrations',
   driver: 'pg',
-  connectionString: (process.env.NODE_ENV === 'test') 
-    ? process.env.TEST_DB_URL 
+  connectionString: (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DB_URL
     : process.env.DB_URL,
   host: process.env.MIGRATION_DB_HOST,
   port: process.env.MIGRATION_DB_PORT,
   database: process.env.MIGRATION_DB_NAME,
   username: process.env.MIGRATION_DB_USER,
-  password: process.env.MIGRATION_DB_PASS
+  password: process.env.MIGRATION_DB_PASS || ''
 };
